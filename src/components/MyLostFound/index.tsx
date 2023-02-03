5383c64a30ceae8fb83fb74ba123a27fdd914687
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-03 15:56:43 +0900

    ⚡️ :: Suspense,ErrorBoundary적용

--- src/components/MyLostFound/index.tsx
+++ src/components/MyLostFound/index.tsx
@@ -1,47 +1,19 @@
-import { useGetMyLostFounds } from "../../quries/lostFound/lostFound.query";
-import MyLostFoundItem from "./MyLostFoundItem";
+import { Suspense } from "react";
+import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
+import NoData from "../Common/NoData";
 import {
   MyLostFoundContainer,
-  MyLostFoundEmptyIcon,
-  MyLostFoundEmptyWrap,
-  MyLostFoundLoadingItem,
 } from "./style";
-import { AiOutlineFolderOpen } from "@react-icons/all-files/ai/AiOutlineFolderOpen";
+import MyLostFoundDetail from "./MyLostFoundDetail";
 
 const MyLostFound = () => {
-  const {
-    data: serverMyLostFoundData,
-    isLoading: serverMyLostFoundDataIsLoading,
-  } = useGetMyLostFounds();
-
-  const loadingItemArray = Array.from({ length: 6 });//배열 6칸짜리 생성
-
   return (
     <MyLostFoundContainer>
-      {serverMyLostFoundDataIsLoading ? (
-        <>
-          {loadingItemArray.map((item, idx) => (
-            <MyLostFoundLoadingItem key={idx} />
-          ))}
-        </>
-      ) : (
-        <>
-          {serverMyLostFoundData?.data.length === 0 ? (
-            <MyLostFoundEmptyWrap>
-              <MyLostFoundEmptyIcon>
-                <AiOutlineFolderOpen />
-              </MyLostFoundEmptyIcon>
-              등록한 분실물이 없습니다
-            </MyLostFoundEmptyWrap>
-          ) : (
-            <>
-              {serverMyLostFoundData?.data.map((lostFound) => (
-                <MyLostFoundItem data={lostFound} key={lostFound.id} />
-              ))}
-            </>
-          )}
-        </>
-      )}
+      <ErrorBoundary fallback={<NoData/>}>
+        <Suspense fallback={<>loading...</>}>
+          <MyLostFoundDetail/>
+        </Suspense>
+      </ErrorBoundary>
     </MyLostFoundContainer>
   );
 };
