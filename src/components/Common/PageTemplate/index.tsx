adb0eee476822b9c142440255d9d3269564da1ff
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-03 15:52:39 +0900

    ⚰️ :: 필요없는 suspense 삭제

--- src/components/Common/PageTemplate/index.tsx
+++ src/components/Common/PageTemplate/index.tsx
@@ -1,10 +1,7 @@
-import React,{ ReactNode, Suspense } from "react";
-import ErrorBoundary from "../../../ErrorBoundary/ErrorBoundary";
-import { useGetMyMember } from "../../../quries/member/member.query";
+import React,{ ReactNode } from "react";
 import DarkModeButton from "../DarkmodeButton";
 import NavBar from "../NavBar";
-import NoData from "../NoData";
-import Spinner from "../Spinner/Spinner";
+
 import {
   PageTemplateContainer,
   PageTemplateContentWrap,
@@ -14,19 +11,16 @@ import {
 interface Props {
   children: ReactNode;
 }
+
 const PageTemplate = ({ children }: Props) => {
   return (
-    <ErrorBoundary fallback={<NoData/>}>
-      <Suspense fallback={<Spinner isLoading={true} isAbsolute={true}/>}>
-        <PageTemplateContainer>
-          <PageTemplateWrap>
-            <NavBar/>
-            <PageTemplateContentWrap>{children}</PageTemplateContentWrap>
-            <DarkModeButton />
-          </PageTemplateWrap>
-        </PageTemplateContainer>
-      </Suspense>
-    </ErrorBoundary>
+    <PageTemplateContainer>
+      <PageTemplateWrap>
+        <NavBar/>
+        <PageTemplateContentWrap>{children}</PageTemplateContentWrap>
+        <DarkModeButton />
+      </PageTemplateWrap>
+    </PageTemplateContainer>
   );
 };
 
