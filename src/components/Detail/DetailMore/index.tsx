055e410896dc14d8176096e15ea96da26c1bea15
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-06 08:45:39 +0900

    ♻️ :: * as S 적용, 쿼리무효화, suspense:true 파라미터 적용

--- src/components/Detail/DetailMore/index.tsx
+++ src/components/Detail/DetailMore/index.tsx
@@ -1,167 +1,72 @@
-import {
-    DetailBottomText,
-    DetailContent,
-    DetailContentCreatedAt,
-    DetailContentTitle,
-    DetailContentWrap,
-    DetailImg,
-    DetailPlaceIcon,
-    DetailPlaceText,
-    DetailPlaceWrap,
-    DetailProfileImg,
-    DetailProfileInfoWrap,
-    DetailProfileName,
-    DetailProfileTextWrap,
-    DetailProfileWrap,
-    DetailWrap,
-} from "../style";
+import * as S from "../style";
 import NoImage from '../.././../assets/image/common/noImage.svg';
 import DefaultProfileImage from "../.././../assets/image/common/defaultProfile.png";
 import TimeCounting from "time-counting";
 import DetailComment from "../DetailComment";
 import { FaMapMarkerAlt } from "@react-icons/all-files/fa/FaMapMarkerAlt";
 import { useGetLostFound } from "../.././../quries/lostFound/lostFound.query";
-import { ParamType } from "../../../types/param/param.type";
-
-export default function DetailMore({lostfoundid}:{lostfoundid:ParamType}){
+import { useQueryClient } from "react-query";
+export default function DetailMore({lostfoundid}:{lostfoundid:string|undefined}){
+    const queryClient = useQueryClient();
+    
+    queryClient.invalidateQueries({
+        queryKey: ["lostFound/getLostFound"],
+        exact: true,
+    })
     const {
         data: serverLostFoundDetailData,
         //isLoading: serverLostFoundDetailDataIsLoading,
-    } = useGetLostFound({ id: Number(lostfoundid)});
+    } = useGetLostFound({ id: Number(lostfoundid)},{suspense:true});
     return(
         <>
             {(
-                <DetailWrap>
-                    <DetailImg src={serverLostFoundDetailData?.data.image || NoImage} />
-                        <DetailProfileWrap>
-                            <DetailProfileInfoWrap>
-                                <DetailProfileImg
+                <S.DetailWrap>
+                    <S.DetailImg src={serverLostFoundDetailData?.data.image || NoImage} />
+                        <S.DetailProfileWrap>
+                            <S.DetailProfileInfoWrap>
+                                <S.DetailProfileImg
                                     src={
                                     serverLostFoundDetailData?.data.member.profileImage ||
                                     DefaultProfileImage
                                     }
                                 />
-                                <DetailProfileTextWrap>
-                                    <DetailProfileName>
+                                <S.DetailProfileTextWrap>
+                                    <S.DetailProfileName>
                                         {serverLostFoundDetailData?.data.member.name}
-                                    </DetailProfileName>
-                                </DetailProfileTextWrap>
-                            </DetailProfileInfoWrap>
-                            <DetailPlaceWrap>
-                                <DetailPlaceText>
+                                    </S.DetailProfileName>
+                                </S.DetailProfileTextWrap>
+                            </S.DetailProfileInfoWrap>
+                            <S.DetailPlaceWrap>
+                                <S.DetailPlaceText>
                                     {serverLostFoundDetailData?.data.place}
-                                </DetailPlaceText>
-                                <DetailPlaceIcon>
+                                </S.DetailPlaceText>
+                                <S.DetailPlaceIcon>
                                     <FaMapMarkerAlt />
-                                </DetailPlaceIcon>
-                            </DetailPlaceWrap>
-                        </DetailProfileWrap>
-                        <DetailContentWrap>
-                            <DetailContentTitle>
+                                </S.DetailPlaceIcon>
+                            </S.DetailPlaceWrap>
+                        </S.DetailProfileWrap>
+                        <S.DetailContentWrap>
+                            <S.DetailContentTitle>
                                 {serverLostFoundDetailData?.data.title}
-                            </DetailContentTitle>
-                            <DetailContentCreatedAt>
+                            </S.DetailContentTitle>
+                            <S.DetailContentCreatedAt>
                                 {TimeCounting(serverLostFoundDetailData?.data.createAt!, {
                                     lang: "ko",
                                 })}
-                            </DetailContentCreatedAt>
-                            <DetailContent>
+                            </S.DetailContentCreatedAt>
+                            <S.DetailContent>
                                 {serverLostFoundDetailData?.data.content}
-                            </DetailContent>
-                            <DetailBottomText>
+                            </S.DetailContent>
+                            <S.DetailBottomText>
                                 {`댓글 ∙ ${serverLostFoundDetailData?.data.comment.length}`}
-                            </DetailBottomText>
-                        </DetailContentWrap>
+                            </S.DetailBottomText>
+                        </S.DetailContentWrap>
                         <DetailComment
                             lostFoundId={Number(lostfoundid)}
                             data={serverLostFoundDetailData?.data.comment!}
                         />
-                </DetailWrap>
+                </S.DetailWrap>
             )}
         </>
     );
 }
-//================================================
-// import {
-//     DetailBottomText,
-//     DetailContent,
-//     DetailContentCreatedAt,
-//     DetailContentTitle,
-//     DetailContentWrap,
-//     DetailImg,
-//     DetailPlaceIcon,
-//     DetailPlaceText,
-//     DetailPlaceWrap,
-//     DetailProfileImg,
-//     DetailProfileInfoWrap,
-//     DetailProfileName,
-//     DetailProfileTextWrap,
-//     DetailProfileWrap,
-//     DetailWrap,
-// } from "../style";
-// import NoImage from '../.././../assets/image/common/noImage.svg';
-// import DefaultProfileImage from "../.././../assets/image/common/defaultProfile.png";
-// import TimeCounting from "time-counting";
-// import DetailComment from "../DetailComment";
-// import { FaMapMarkerAlt } from "@react-icons/all-files/fa/FaMapMarkerAlt";
-// import { useGetLostFound } from "../.././../quries/lostFound/lostFound.query";
-// import { ParamType } from "../../../types/param/param.type";
-
-// export default function DetailMore({lostfoundid}:{lostfoundid:ParamType}){
-//     const {
-//         data: serverLostFoundDetailData,
-//         isLoading: serverLostFoundDetailDataIsLoading,
-//     } = useGetLostFound({ id: Number(lostfoundid)});
-//     return(
-//         <>
-//             {!serverLostFoundDetailDataIsLoading && (
-//                 <DetailWrap>
-//                     <DetailImg src={serverLostFoundDetailData?.data.image || NoImage} />
-//                         <DetailProfileWrap>
-//                             <DetailProfileInfoWrap>
-//                                 <DetailProfileImg
-//                                     src={
-//                                     serverLostFoundDetailData?.data.member.profileImage ||
-//                                     DefaultProfileImage
-//                                     }
-//                                 />
-//                                 <DetailProfileTextWrap>
-//                                     <DetailProfileName>
-//                                         {serverLostFoundDetailData?.data.member.name}
-//                                     </DetailProfileName>
-//                                 </DetailProfileTextWrap>
-//                             </DetailProfileInfoWrap>
-//                             <DetailPlaceWrap>
-//                                 <DetailPlaceText>
-//                                     {serverLostFoundDetailData?.data.place}
-//                                 </DetailPlaceText>
-//                                 <DetailPlaceIcon>
-//                                     <FaMapMarkerAlt />
-//                                 </DetailPlaceIcon>
-//                             </DetailPlaceWrap>
-//                         </DetailProfileWrap>
-//                         <DetailContentWrap>
-//                             <DetailContentTitle>
-//                                 {serverLostFoundDetailData?.data.title}
-//                             </DetailContentTitle>
-//                             <DetailContentCreatedAt>
-//                                 {TimeCounting(serverLostFoundDetailData?.data.createAt!, {
-//                                     lang: "ko",
-//                                 })}
-//                             </DetailContentCreatedAt>
-//                             <DetailContent>
-//                                 {serverLostFoundDetailData?.data.content}
-//                             </DetailContent>
-//                             <DetailBottomText>
-//                                 {`댓글 ∙ ${serverLostFoundDetailData?.data.comment.length}`}
-//                             </DetailBottomText>
-//                         </DetailContentWrap>
-//                         <DetailComment
-//                             lostFoundId={Number(lostfoundid)}
-//                             data={serverLostFoundDetailData?.data.comment!}
-//                         />
-//                 </DetailWrap>
-//             )}
-//         </>
-//     );
-// }
