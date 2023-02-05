e8bd493a9a09de658e40ece957060b6c31ba054b
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-02-06 08:48:13 +0900

    ⚰️ :: 안쓰는 임포트 삭제

--- src/components/MyLostFound/index.tsx
+++ src/components/MyLostFound/index.tsx
@@ -1,16 +1,17 @@
 import { Suspense } from "react";
-import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
-import NoData from "../Common/NoData";
+import ErrorBoundary from "../Common/ErrorBoundary/ErrorBoundary";
 import {
   MyLostFoundContainer,
 } from "./style";
 import MyLostFoundDetail from "./MyLostFoundDetail";
+import FallbackSkeleton from "../Common/FallbackSkeleton";
+import NoData from "../Common/NoData";
 
 const MyLostFound = () => {
   return (
     <MyLostFoundContainer>
-      <ErrorBoundary fallback={<NoData/>}>
-        <Suspense fallback={<>loading...</>}>
+      <ErrorBoundary fallback={<h1 style={{ fontSize: "20px" }}>Error :( </h1>}>
+        <Suspense fallback={<FallbackSkeleton/>}>
           <MyLostFoundDetail/>
         </Suspense>
       </ErrorBoundary>
